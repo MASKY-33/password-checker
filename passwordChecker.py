0729ed24621aebcbd28211bd938888afe740193e
MASKY-33 <masky.somebody33@gmail.com>
2026-06-06 21:11:10 +0200

    Create passwordChecker.py

---
+++ passwordChecker.py
@@ -0,0 +1,7 @@
+password_check = input("Put in a password: ")
+password_equal_to = "Python33"
+
+if password_check == password_equal_to:
+    print("Access succeed")
+else:
+    print("Access denied")
